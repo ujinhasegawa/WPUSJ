f76fcea21593953a1a6ba3b4185d16fae8db474e
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2017-08-16 00:53:52 +0900

    ユーザコントローラのaction(new, create, index)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,25 @@
 class UsersController < ApplicationController
 
 	def new
+	  @user = User.new
 	end
 
 	def show
 	end
 
 	def index
+	  @users = User.all
 	end
 
 	def create
+	  @user = User.new(user_params)
+	  @user.save
+	  redirect_to controller: 'users', action: 'index'
 	end
 
+	private
+
+	def user_params
+	  params.require(:user).permit(:name)
+	end
 end
