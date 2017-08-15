1e2eed3482a2671335d19488b9182b5a38d09c07
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2017-08-16 01:20:47 +0900

    userテーブルマイグレイトファイルに国と地域を追加

--- db/migrate/20170812015302_create_users.rb
+++ db/migrate/20170812015302_create_users.rb
@@ -2,7 +2,9 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string  :name
-      t.string  :group_name
+      t.string  :country
+      t.string  :area
+      t.integer :group_id
       t.integer :point_month
       t.integer :point_year
       t.integer :point_lifetime
