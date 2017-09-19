878d865f82983954653a741cb66859112dc4f0d9
iso-toshihiro <t_adventure@yahoo.co.jp>
2017-09-19 17:48:51 +0100

    モデルAuthentication作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170812015302) do
+ActiveRecord::Schema.define(version: 20170919164157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentications", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "token", null: false
+    t.datetime "deadline", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
