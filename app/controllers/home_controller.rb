2897ddbe9ac7dd13f91cdf98ec9c4a26ee091674
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2017-09-23 20:51:20 +0900

    ユーザサインアップ機能拡張

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+		# myユーザの簡単な情報
+		# タイムライン
+		# リンク
+  end
+
+  def my_page
+		# 私のステータス
+		# 活動履歴
+		# リンク
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessor :name
+
+  has_one :information
+  accepts_nested_attributes_for :information
 end
