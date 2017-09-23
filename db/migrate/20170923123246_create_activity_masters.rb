066e2b0641f5f5b1fdc4fd0a5888d862931c06bb
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2017-09-23 21:58:30 +0900

    活動マスタテーブル作成

--- db/migrate/20170923123246_create_activity_masters.rb
+++ db/migrate/20170923123246_create_activity_masters.rb
@@ -1,6 +1,10 @@
 class CreateActivityMasters < ActiveRecord::Migration[5.1]
   def change
     create_table :activity_masters do |t|
+    	t.string  :title
+    	t.text    :detail
+    	t.integer :group
+    	t.integer :point
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170923092502) do
+ActiveRecord::Schema.define(version: 20170923123246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activity_masters", force: :cascade do |t|
+    t.string "title"
+    t.text "detail"
+    t.integer "group"
+    t.integer "point"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "information", force: :cascade do |t|
     t.bigint "user_id"
