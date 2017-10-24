f4c50ae734789a1685d72de8f6d97a9969e89171
iso-toshihiro <t_adventure@yahoo.co.jp>
2017-10-24 17:52:42 +0100

    アクティビティ登録画面作成とルート作成

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,20 @@
 class ActivitiesController < ApplicationController
+
+  def new
+    @activity        = Activity.new
+    @activity_master = ActivityMaster.all
+  end
+
+  def create
+    params[:activity][:achieve_at] = params[:activity][:achieve_at].to_datetime
+    @activity = current_user.activity.new(activity_params)
+    @activity.save
+    redirect_to :root
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:title, :memo, :achieve_at)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+  get  'home/index'
+
   # devise_for :users
   devise_for :users, :controllers => {
     :registrations => "users/registrations"
   }
-  get 'home/index'
-  root 'home#index'
+
+  resources :activities
 end
