98d5e1fb78b960d11a526aaffb25730c8e26a883
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2017-10-25 22:19:12 +0900

    アクティビティマスタからの登録ができるようにした

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,20 +1,38 @@
 class ActivitiesController < ApplicationController
 
   def new
-    @activity        = Activity.new
+    if params[:activity].nil?
+      @activity = Activity.new
+      @disabled = false
+    else
+      id = params[:activity].to_i
+      @activity = Activity.new(title: ActivityMaster.find(id).title, id: id)
+      @disabled = true
+    end
     @activity_master = ActivityMaster.all
   end
 
   def create
     params[:activity][:achieve_at] = params[:activity][:achieve_at].to_datetime
+    id = params[:activity][:id]
+
+    if not id.empty?
+      if ActivityMaster.find(id).title == params[:activity][:title] # データが改ざんされていないかチェック
+        params[:activity][:point] = ActivityMaster.find(id).point
+        params[:activity][:group] = ActivityMaster.find(id).group
+      else
+        redirect_to :new_activity and return
+      end
+    end
+
     @activity = current_user.activity.new(activity_params)
     @activity.save
-    redirect_to :root
+    redirect_to :new_activity
   end
 
   private
 
   def activity_params
-    params.require(:activity).permit(:title, :memo, :achieve_at)
+    params.require(:activity).permit(:title, :memo, :point, :group, :achieve_at)
   end
 end
