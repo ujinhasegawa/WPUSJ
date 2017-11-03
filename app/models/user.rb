c5063c7a914b2d50d177a4c9f84c255fb71efc4e
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2017-11-03 16:58:51 +0900

    ユーザモデル 不要なアクセサ削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessor :name
-
   has_one :information
   has_many :activity
   accepts_nested_attributes_for :information
