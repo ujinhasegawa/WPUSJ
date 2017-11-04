3ba8d477a2a9088fbee78555d9f3a3759f36bf09
iso-toshihiro <iso_toshihiro@nexway.co.jp>
2017-11-04 15:34:13 +0900

    モデル内でリレーションを作成

--- app/models/area_master.rb
+++ app/models/area_master.rb
@@ -1,2 +1,3 @@
 class AreaMaster < ApplicationRecord
+	belongs_to :country_master, dependent: :destroy
 end
--- app/models/country_master.rb
+++ app/models/country_master.rb
@@ -1,2 +1,4 @@
 class CountryMaster < ApplicationRecord
+	has_many :area_master
+	accepts_nested_attributes_for :area_master
 end
